521bbcdaf0a49da7ab533fff2e15e424ddd49766
SeungBin Kim <luxorienskim@gmail.com>
2023-02-01 09:24:44 +0900

    test: add 404 case

--- server_test.go
+++ server_test.go
@@ -41,6 +41,20 @@ func TestGETPlayers(t *testing.T) {
 
 		assertResponseBody(t, response.Body.String(), "10")
 	})
+
+	t.Run("returns 404 on missing player", func(t *testing.T) {
+		request := newGetScoreRequest("Apollo")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		got := response.Code
+		want := http.StatusNotFound
+
+		if got != want {
+			t.Errorf("got statud %d want %d", got, want)
+		}
+	})
 }
 
 func newGetScoreRequest(name string) *http.Request {
