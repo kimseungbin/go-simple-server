e5354db3f45a4fdd59ea4bfac771c46d942be1f0
SeungBin Kim <luxorienskim@gmail.com>
2023-02-01 11:58:57 +0900

    test: requested player in win call

--- server_test.go
+++ server_test.go
@@ -76,6 +76,10 @@ func TestStoreWins(t *testing.T) {
 		if len(store.winCalls) != 1 {
 			t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
 		}
+
+		if store.winCalls[0] != player {
+			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
+		}
 	})
 }
 
